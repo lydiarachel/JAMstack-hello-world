1f0915b81bb7ac64f5b1f46f3f77d33697683deb
lydiarachel <lydiarachel@gmail.com>
2018-09-08 10:36:27 -0400

    first commit for hackathon project

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Lydia Velasquez.</p>
+    <p>I am so excited to join my first Hackathon.  And what a treat! It's in San Francisco!!!!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
